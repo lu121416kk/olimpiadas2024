4b9db1c3d4cd09da361ec9877cee9613ad819383
lu121416kk <146039249+lu121416kk@users.noreply.github.com>
2024-09-27 11:55:53 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,15 @@
+var ImagemAtual = "img/menu_burguer";
+var ImagemProxima = "img/menuburguer.png";
+var ImagemAtual2 = "img/logo_menu.png";
+var ImagemProxima2 = "img/logomenu.png";
+
 window.addEventListener("scroll", function(){
     let header = document.querySelector('header')
     header.classList.toggle('rolagem', window.scrollY > 0)
+    document.getElementById("dentro-menu").src = ImagemAtual;
+    ImagemAtual = ImagemProxima;
+    document.getElementById("icone").src = ImagemAtual2;
+    ImagemAtual2 = ImagemProxima2;
 })
 
 document.querySelectorAll('.navmenu a').forEach(link => {
