881f99a03bcd885f682bde217eb237770da49a89
lu121416kk <146039249+lu121416kk@users.noreply.github.com>
2024-10-16 13:20:37 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -42,3 +42,29 @@ async function loadMedalsData() {
 // Carregar os dados quando a página for carregada
 window.onload = loadMedalsData;
 
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
+
+document.querySelectorAll('.navmenu a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementsByClassName('menu-toggle').checked = false;
+    });
+});
+
+function toggleMenu() {
+    var navMenu = document.querySelector('.navmenu');
+    navMenu.classList.toggle('active');
+}
+
+document.querySelectorAll('.navmenu li').forEach(link => {
+    link.addEventListener('click', () => {
+        var navMenu = document.querySelector('.navmenu');
+        navMenu.classList.remove('active');
+    });
+});
+
+
+
